f8ddf77ccd29469aba258c7fc85cf48d3f1eaf61
haja rand <hrandrianarijaona@excilys.com>
2015-07-13 14:54:58 +0200

    Gestion d'erreur interne => status code 500

--- src/main/java/com/excilys/formation/controller/impl/ExceptionHandlingController.java
+++ src/main/java/com/excilys/formation/controller/impl/ExceptionHandlingController.java
@@ -12,9 +12,16 @@ public class ExceptionHandlingController {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)  // 400
     @ExceptionHandler(BadParamException.class)
-    public void handleConflict() {
+    public void handleBadParamConflict() {
         // Nothing to do
     	System.out.println("bad request");
+    }
+	
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 500
+    @ExceptionHandler(Exception.class)
+    public void handleServerConflict() {
+        // Nothing to do
+    	System.out.println("pb server");
     }
 	
 }
