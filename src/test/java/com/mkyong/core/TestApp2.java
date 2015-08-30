01afebb4a452a8c7f78b2792b4d051327b6259b1
hrandr <hrandrianarijaona@excilys.com>
2015-08-30 16:03:34 +0200

    implémentation test v1

---
+++ src/test/java/com/mkyong/core/TestApp2.java
@@ -0,0 +1,15 @@
+package com.mkyong.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestApp2 {
+
+	@Test
+	public void testPrintHelloWorld2() {
+
+		Assert.assertEquals(App.getHelloWorld2(), "Hello World 2");
+
+	}
+
+}
